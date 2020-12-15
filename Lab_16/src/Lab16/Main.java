bfbd17becb5d3f7ff405ed189efe06ebb6a387a9
Волщуков Роман <70647680+No-l1k@users.noreply.github.com>
2020-12-15 12:32:47 +0300

    Add files via upload

---
+++ Lab_16/src/Lab16/Main.java
@@ -0,0 +1,16 @@
+package Lab16;
+
+public class Main {
+    public static final String[] STRINGS = {"(1+8)–9/4", "6/5–2*9 "};
+
+    public static void main(String[] args) {
+        for (String str : STRINGS) {
+            System.out.print(str + ": ");
+            System.out.println(isCorrect(str) ? "False" : "True");
+        }
+    }
+
+    public static boolean isCorrect(String str) {
+        return str.matches(".*\\d+.*") && !str.matches(".*\\d+\\+.*");
+    }
+}
